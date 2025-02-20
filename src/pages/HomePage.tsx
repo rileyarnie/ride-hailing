8ec34cb1fe1e2149d2a9ab66fdc613a7b0a2c76e
rileyarnie <arnold.ril3y@gmail.com>
2025-02-20 23:36:49 +0300

    resolve import issues

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import { Button } from "../components/ui/button";
+
+const HomePage = () => {
+  return (
+    <div>
+      HomePage
+      <Button>Hello button</Button>
+    </div>
+  );
+};
+
+export default HomePage;
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import path from "path";
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +12,9 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
