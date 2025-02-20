d3c82ffbb153f03817881a134b070c595f6ffd2d
rileyarnie <arnold.ril3y@gmail.com>
2025-02-20 19:09:30 +0300

    only show devtools in development

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -10,7 +10,7 @@ function RootComponent() {
   return (
     <React.Fragment>
       <Outlet />
-      <TanStackRouterDevtools />
+      {import.meta.env.MODE === "development" && <TanStackRouterDevtools />}
     </React.Fragment>
   );
 }
